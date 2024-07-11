2ddc7cff72975ccdf58e6e185e843e821f8afaa1
Shahrukh <shahrukh1494@gmail.com>
2024-07-11 17:14:23 +0530

    formik initilValues, onChange and values

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -1,24 +1,50 @@
 import { useFormik } from "formik";
 
 const YoutubeForm = () => {
-  const formik = useFormik();
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      channel: "",
+    },
+  });
+
+  console.log("values", formik.values);
 
   return (
     <div>
-        <form>
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="name" />
+      <form>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
 
-            <label htmlFor="email">E-mail</label>
-            <input type="email" id="email" name="email" />
+        <label htmlFor="email">E-mail</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
 
-            <label htmlFor="channel">Channel</label>
-            <input type="text" id="channel" name="channel" />
+        <label htmlFor="channel">Channel</label>
+        <input
+          type="text"
+          id="channel"
+          name="channel"
+          onChange={formik.handleChange}
+          value={formik.values.channel}
+        />
 
-            <button>Submit</button>
-        </form>
+        <button>Submit</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default YoutubeForm
+export default YoutubeForm;
