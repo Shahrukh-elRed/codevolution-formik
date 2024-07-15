df8875938b84de7944555c3f15fec942cf4a1aaa
Shahrukh <shahrukh1494@gmail.com>
2024-07-15 16:25:08 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import './App.css';
-import YoutubeForm from './components/YoutubeForm';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <YoutubeForm />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
