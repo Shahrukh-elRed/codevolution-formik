df6931ab87d8cfa40fbadb79cdbde422a007c575
Shahrukh <shahrukh1494@gmail.com>
2024-07-15 16:35:43 +0530

    building reusable formik controls

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import FormikContainer from "./components/FormikContainer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <FormikContainer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/FormikContainer.js
@@ -0,0 +1,26 @@
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
+
+const FormikContainer = () => {
+  const initialValues = {};
+  const validationSchema = Yup.object({});
+  const onSubmit = (values) => console.log("Form data", values);
+
+  return (
+    <div>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        {(formik) => (
+          <Form>
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default FormikContainer;
