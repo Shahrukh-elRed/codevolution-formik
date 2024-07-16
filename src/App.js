0b6ddaff874584cbbee6c52e00058fcc54a864f1
Shahrukh <shahrukh1494@gmail.com>
2024-07-16 18:24:30 +0530

    course enrollment form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
+import EnrollmentForm from "./components/EnrollmentForm";
 // import LoginForm from "./components/LoginForm";
-import RegistrationForm from "./RegistrationForm";
+// import RegistrationForm from "./RegistrationForm";
 
 function App() {
   return (
     <div className="App">
       {/* <LoginForm /> */}
-      <RegistrationForm />
+      {/* <RegistrationForm /> */}
+      <EnrollmentForm />
     </div>
   );
 }
