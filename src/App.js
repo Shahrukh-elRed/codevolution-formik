84436aacde1533cdfd967c88654013930d13c11e
Shahrukh <shahrukh1494@gmail.com>
2024-07-16 18:52:48 +0530

    add chakra-ui library

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
 import EnrollmentForm from "./components/EnrollmentForm";
+import { ChakraProvider } from "@chakra-ui/react";
 // import LoginForm from "./components/LoginForm";
 // import RegistrationForm from "./RegistrationForm";
 
 function App() {
   return (
-    <div className="App">
-      {/* <LoginForm /> */}
-      {/* <RegistrationForm /> */}
-      <EnrollmentForm />
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        {/* <LoginForm /> */}
+        {/* <RegistrationForm /> */}
+        <EnrollmentForm />
+      </div>
+    </ChakraProvider>
   );
 }
 
